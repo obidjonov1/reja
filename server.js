59a56242281a894464c61ff56e0f4be2db802bcf
obidjonov1 <obidjonov@naver.com>
2023-01-02 03:34:36 +0900

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,30 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 -> Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2 -> Sessiom
+
+// 3 -> View code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 -> Routing code (serverga qo'shish)
+app.get("/hello", function (req, res) {
+  res.end("hello world");
+});
+
+app.get("/gift", function (req, res) {
+  res.end("Siz sovg'alar bo'limidasiz");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`The server running successfully on port: ${PORT}`);
+});
