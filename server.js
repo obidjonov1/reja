d45b78ce66b96f0d7412e189e315b1dc9312b69d
obidjonov1 <obidjonov@naver.com>
2023-01-05 20:03:59 +0900

    feat: develop author portfolio page

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
-console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-// 1 -> Kirish code
-app.use(express.static("public")); 
-app.use(express.json()); // expressdan kelayotgan objectni jsonga o'tkazib oldik
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// 1-> kirish
+app.use(express.static("public"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2 -> Sessiom
+// 2-> session
 
-// 3 -> View code
+// 3 -> view code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// 4 -> Routing code (serverga qo'shish)
-app.get("/hello", function (req, res) {
-  res.end("hello world");
+// 4 -> routing code
+app.post("/create-item", (req, res) => {
+  // console.log(req.body);
+  // res.json({ test: "sucsess" });
+});
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
-app.get("/gift", function (req, res) {
-  res.end("Siz sovg'alar bo'limidasiz");
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The server running successfully on port: ${PORT}`);
+  console.log(`runnig syccessfuly ${PORT}`);
 });
